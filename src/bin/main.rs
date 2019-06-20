80d82e6480f086306c9f0ec4ef7dc6d6726ede86
Oscar <osmithsi@sfu.ca>
2019-06-20 00:45:45 -0700

    First version of tokenizer complete

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::env;
+use std::fs;
+
+use yoloxide::tokenizer;
+
+fn main()
+{
     println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    // Take the first argument as a file path and read it for yolol code
+    let yolol_code = fs::read_to_string(args[1].clone()).unwrap();
+    println!("{}", yolol_code);
+    println!("{:?}", tokenizer::tokenize(yolol_code));
 }
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod tokenizer;
+
+pub fn helloWorld()
+{
+    println!("Testing time");
+}
